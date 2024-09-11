4894c49e6a68319cd03a7b8f11fa0fe92d6cb958
tubi55 <tubi55@nate.com>
2024-09-11 10:11:02 +0900

    자바스크립트로 HTML요소 선택하는 법

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-console.log("test");
-//크롬 브라우저에서 f12 (개발자도구)
-/*
-  DOM (Document Object Model) 문서 객체 모델
-*/
+//단수의 html요소 선택시
+const h1 = document.querySelector("h1");
+console.log(h1);
+//console.dir(h1);
 
-const el = document.querySelector("h1");
-console.log(el);
+//특정 부모요소 안쪽의 자식 요소 탐색 선택
+const title = document.querySelector(".title a");
+console.log(title);
+
+//복수개 요소 탐색하는 방법
+const lis = document.querySelectorAll("ul li");
+
+//배열형태로 반환된 복수개의 html요소 하나씩 선택
+for (let i = 0; i < 4; i++) {
+  console.log(lis[i]);
+}
