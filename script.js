755fd317ee4a976e11b612f3ce056c10386114de
vboxuser <tspartner21@gmail.com>
2024-09-11 10:42:45 +0900

    함수 - 일반/익명/화살표

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ const lis = document.querySelectorAll("ul li");
 for (let i = 0; i < 4; i++) {
   console.log(lis[i]);
 }
+//자바스크립트에서 함수 만드는 법
+//1. 함수선언식
+function plus(num1, num2){
+  const result  = num1 + num2;
+  return result;
+}
+
+console.log(plus(2, 4));
+
+//2. 함수표현식(변수에 익명함수 대입)
+const minus = function(num1, num2){
+  const result = num1 - num2;
+  return result;
+};
+
+console.log(minus(3,1));
+
+//3. 화살표 함수로 축약된 함수 표현식
+//화살표 함수에서는 function키워드 생략, 중괄호 안쪽에 return문만 있으면 중괄호와 return키워드 생략
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+console.log(multiply(3, 3));
+
+const multiply2 = (num1, num2) => num1 * num2;
+
+console.log(multiply(4, 3));
